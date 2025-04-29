563baecc572b9cffd94eb3bf62e08578124e5431
Karthik-bn1993 <karthik.neelakanta1993@gmail.com>
2025-04-29 14:52:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "self-hosted-runner" {
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.7.0"
 
   bucket = "testingmodulebucket"
   acl    = "private"
