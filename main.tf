23fc521726cd4a99fc6314e86d660135c0ea043e
Karthik-bn1993 <karthik.neelakanta1993@gmail.com>
2025-04-29 14:56:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "4.7.0"
 
-  bucket = "testingmodulebucket"
+  bucket = "testingmodulebucket270425"
   acl    = "private"
 
   versioning = {
